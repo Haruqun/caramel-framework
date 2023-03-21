b15860137a3c0c1e625bedc26455e58fe75aa420
Haruqun <info@fivenine-design.com>
2023-03-21 20:12:34 +0900

    デザイン変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="/css/custom.scss">
+    <link rel="stylesheet" href="/css/custom.css">
 
 
     <title>Hello, world!</title>
@@ -116,8 +116,11 @@
                     LapinBOTは、安心して利用できる自動ベッドツールです。プレイヤーの負担を軽減し、より効率的なプレイを実現するために開発されました。Lapinbetカジノでのプレイをより楽しく、より収益性の高いものにするために、LapinBOTをぜひご活用ください。
                 </p>
                 <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">購入手続き</button>
-                    <button type="button" class="btn btn-secondary btn-lg px-4">お問い合わせ</button>
+                    <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">
+                        <i class="bi bi-bag-fill"></i> 購入手続き
+                    </button>
+                    <button type="button" class="btn btn-secondary btn-lg px-4"><i
+                            class="bi bi-envelope-paper-heart-fill"></i> お問い合わせ</button>
                 </div>
             </div>
             <div class="" style="">
@@ -134,31 +137,49 @@
             </div>
         </div>
     </div>
-    <div class="container marketing py-5">
+    <div class="container py-5">
 
         <div class="row featurette align-items-center">
-            <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading">
+            <div class="col-md-12 order-md-2">
+                <h1 class="featurette-heading">
+                    <i class="bi bi-coin"></i>
                     ロジックに従って自動ベット
-                </h2>
+                </h1>
                 <p class="lead">
                     LapinBOTは、自動ベッドツールとして、Lapinbetカジノで利用できます。<br>
                     様々なアルゴリズムに基づいたベットを行い、現在６種類のベットロジックが収録されています。代表的なマーチンゲール法・ウィナーズ法も使うことができます。
                 </p>
-                <h2 class="featurette-heading">
-                    利益を確保するために
-                </h2>
-                <p class="lead">
-                    ユニット数を設定することで、自分の好きな枚数から自動ベットを開始することができ、大きな金額でベットを行うことも可能です。さらに、利益確定機能を搭載しているため、指定した利益確定でゲームを終了することができます。ロスカット機能もあるため、所持金が指定した金額に達した段階で自動的にゲームを終了することができます。
-                </p>
-            </div>
-            <div class="col-md-5 order-md-1">
-                <img src="https://placehold.jp/500x500.png" class="img-fluid border rounded-3 shadow-lg mb-4"
-                    alt="Example image" width="500" height="500" loading="lazy">
             </div>
         </div>
     </div>
-    <div class="container-fluid gray-bg py-5">
+
+    <div class="container-fluid gray-bg  py-5">
+        <div class="container">
+            <div class="row featurette align-items-center">
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading">
+                        ロジックに従って自動ベット
+                    </h2>
+                    <p class="lead">
+                        LapinBOTは、自動ベッドツールとして、Lapinbetカジノで利用できます。<br>
+                        様々なアルゴリズムに基づいたベットを行い、現在６種類のベットロジックが収録されています。代表的なマーチンゲール法・ウィナーズ法も使うことができます。
+                    </p>
+                    <h2 class="featurette-heading">
+                        利益を確保するために
+                    </h2>
+                    <p class="lead">
+                        ユニット数を設定することで、自分の好きな枚数から自動ベットを開始することができ、大きな金額でベットを行うことも可能です。さらに、利益確定機能を搭載しているため、指定した利益確定でゲームを終了することができます。ロスカット機能もあるため、所持金が指定した金額に達した段階で自動的にゲームを終了することができます。
+                    </p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img src="https://placehold.jp/500x500.png" class="img-fluid border rounded-3 shadow-lg mb-4"
+                        alt="Example image" width="500" height="500" loading="lazy">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid py-5">
 
         <div class="container">
             <div class="row featurette align-items-center">
@@ -177,13 +198,38 @@
                         ユニット数を設定することで、自分の好きな枚数から自動ベットを開始することができ、大きな金額でベットを行うことも可能です。さらに、利益確定機能を搭載しているため、指定した利益確定でゲームを終了することができます。ロスカット機能もあるため、所持金が指定した金額に達した段階で自動的にゲームを終了することができます。
                     </p>
                 </div>
-                <div class="col-md-5 order-md-2">
+                <div class="col-md-5 order-md-2 my-5">
                     <img src="https://placehold.jp/500x500.png" class="img-fluid border rounded-3 shadow-lg mb-4"
                         alt="Example image" width="500" height="500" loading="lazy">
                 </div>
             </div>
         </div>
     </div>
+    <div class="container-fluid gold-bg py-5">
+        <div class="container">
+
+            <div class="row featurette align-items-center">
+                <div class="col-md-6">
+                    <h2 class="featurette-heading">
+                        ロジックに従って自動ベット
+                    </h2>
+                    <p class="lead">
+                        LapinBOTは、自動ベッドツールとして、Lapinbetカジノで利用できます。<br>
+                        様々なアルゴリズムに基づいたベットを行い、現在６種類のベットロジックが収録されています。代表的なマーチンゲール法・ウィナーズ法も使うことができます。
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <h2 class="featurette-heading">
+                        利益を確保するために
+                    </h2>
+                    <p class="lead">
+                        ユニット数を設定することで、自分の好きな枚数から自動ベットを開始することができ、大きな金額でベットを行うことも可能です。さらに、利益確定機能を搭載しているため、指定した利益確定でゲームを終了することができます。ロスカット機能もあるため、所持金が指定した金額に達した段階で自動的にゲームを終了することができます。
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
